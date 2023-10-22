6922a7972346bbbd90a8ae9c2555d4c9b06c7d7b
thihaeung <124340704+thihaeungg@users.noreply.github.com>
2023-10-23 02:32:26 +0630

    Fixed Trait

--- src/Trait/CommandMapTrait.php
+++ src/Trait/CommandMapTrait.php
@@ -9,28 +9,31 @@ use ThihaMorph\MyanMap\Eloquent\Township;
 
 trait CommandMapTrait
 {
-    public function create(string $value,string $model,array $subValue = [])
+    public function create(string $value,array $subValue = [])
     {
+        $model = get_class($this);
+
         $value ? $model::firstOrCreate([$value]) : throw new MyanMapException('တန်ဖိုး ထည့်ရပါမည်။');
 
-        $this->createValidationAndSave();
+        $this->createValidationAndSave($model, $subValue);
     }
 
-    public function createValidationAndSave($model, $subValue){
+    public function createValidationAndSave($model, $subValue)
+    {
             switch ($model) {
-                case 'state':
+                case State::class:
                     if(!$this->areAllIntegers($subValue) && count($subValue) > 0){
                         throw new MyanMapException('Array ထဲတွင် တန်ဖိုးရှိရမည်ဖြစ်ပြီး တန်ဖိုးအားလုံးသည် ကိန်းပြည့်ဖြစ်ရမည်။');
                     }
                     $model->cities()->attach($subValue);
                     break;
-                case 'city':
+                case City::class:
                     if(!$this->areAllIntegers($subValue) && count($subValue) > 0){
                         throw new MyanMapException('Array ထဲတွင် တန်ဖိုးရှိရမည်ဖြစ်ပြီး တန်ဖိုးအားလုံးသည် ကိန်းပြည့်ဖြစ်ရမည်။');
                     }
                     $model->townships()->attach($subValue);
                     break;
-                case 'township':
+                case Township::class:
                     break;
                 default:
                     throw new MyanMapException('မြို့နယ်တွင် ချိတ်စရာ Array မရှိပါ။');
@@ -40,27 +43,30 @@ trait CommandMapTrait
 
     public function update(string $newValue, string $model,array $subValue = [],int $id)
     {
+        $model = get_class($this);
+
         $model-findOrFail($id);
         $newValue ? $model::update([$newValue]) : throw new MyanMapException('တန်ဖိုး ထည့်ရပါမည်။');
 
-        $this->updateValidationAndSave();
+        $this->updateValidationAndSave($model, $subValue);
     }
 
-    public function updateValidationAndSave($model, $subValue){
+    public function updateValidationAndSave($model, $subValue)
+    {
         switch ($model) {
-            case 'state':
+            case State::class:
                 if(!$this->areAllIntegers($subValue) && count($subValue) > 0){
                     throw new MyanMapException('Array ထဲတွင် တန်ဖိုးရှိရမည်ဖြစ်ပြီး တန်ဖိုးအားလုံးသည် ကိန်းပြည့်ဖြစ်ရမည်။');
                 }
                 $model->cities()->updateExistingPivot($subValue);
                 break;
-            case 'city':
+            case City::class:
                 if(!$this->areAllIntegers($subValue) && count($subValue) > 0){
                     throw new MyanMapException('Array ထဲတွင် တန်ဖိုးရှိရမည်ဖြစ်ပြီး တန်ဖိုးအားလုံးသည် ကိန်းပြည့်ဖြစ်ရမည်။');
                 }
                 $model->townships()->updateExistingPivot($subValue);
                 break;
-            case 'township':
+            case Township::class:
                 break;
             default:
                 throw new MyanMapException('မြို့နယ်တွင် ချိတ်စရာ Array မရှိပါ။');
@@ -70,11 +76,14 @@ trait CommandMapTrait
 
     public function delete(int $id)
     {
-        $model-findOrFail($id);
+        $model = get_class($this);
+
+        $model::findOrFail($id);
         $model->delete();
     }
 
-    function areAllIntegers($array) {
+    function areAllIntegers($array)
+    {
         foreach ($array as $value) {
             if (!is_int($value)) {
                 return false;
